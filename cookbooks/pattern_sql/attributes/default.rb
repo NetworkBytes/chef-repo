5ac76f2fffc21a3ac7b92d2942e518ac36f304a6
John Bencic <jbencic@users.noreply.github.com>
2017-06-05 15:13:16 +1000

    Update default.rb

--- cookbooks/pattern_sql/attributes/default.rb
+++ cookbooks/pattern_sql/attributes/default.rb
@@ -1,5 +1,7 @@
 
 
+default['pattern_sql']['download_url'] = 'http://download.microsoft.com/download/F/E/D/FEDB200F-DE2A-46D8-B661-D019DFE9D470/ENU/x64'
+
 default['sql_server']['accept_eula'] = true
 default['sql_server']['product_key'] = nil
 default['sql_server']['version'] = '2012'
